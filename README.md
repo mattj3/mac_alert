ae9ab82302bc369e661fb406187336b1ae3e055c
Matthew Johnson <matthewjohnson@Matthews-MacBook-Pro-2.local>
2021-07-22 13:49:03 -0500

    small changes

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
 
 ## References
 - https://stackoverflow.com/questions/17651017/python-post-osx-notification
+
+## Feature Improvement Ideas 
+- Add audible alert 
+- Containerize/playbook (python3, pip3, subprocess, requests)
+- Linux version
+- Add in `str(r.elapsed.total_seconds())` for longer 200 responses
+
+## Optional 
+```#Use below if you want an alert even if there are no issues, can be annoying if running frequently -MJ
+# if (notification_message == ""):
+#     notification_message = "No issues found! Get some coffee!"
+```
--- macAlert.py
+++ macAlert.py
@@ -1,22 +1,23 @@
 import subprocess
 import requests
 
-urls = ["https://www.google.com/", "https://www.linkedin.com/"]
-CMD = '''
+urls = ["https://release.clx.lawgix-dev.com/", "https://www.linkedin.com/"]
+
+appleScriptCommand = '''
 on run argv
   display notification (item 2 of argv) with title (item 1 of argv)
 end run
 '''
 
 def notify(title, text):
-  subprocess.call(['osascript', '-e', CMD, title, text])
+  subprocess.call(['osascript', '-e', appleScriptCommand, title, text])
 
 def url_health_check(url_list):
     notification_message = ""
     for url in url_list:
         r = requests.get(url)
         if str(r.status_code) != "200":
-            notification_message += str(r.status_code) + "   " + url + "\n"
+            notification_message += str(r.status_code) + "  " + url + "\n"
             notify("Site Health Check Alert", notification_message)
 
     #Use below if you want an alert even if there are no issues, can be annoying if running frequently -MJ
